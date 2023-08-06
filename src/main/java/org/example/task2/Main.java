573ddebe254cda2e8021ce7d7b1bb41d464278b0
Evgeniy Pikin <pikin@bk.ru>
2023-08-06 13:59:10 +0500

    List and Map added

--- src/main/java/org/example/task2/Main.java
+++ src/main/java/org/example/task2/Main.java
@@ -1,2 +1,17 @@
-package org.example.task2;public class Main {
+package org.example.task2;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    List<String> names = new ArrayList<>(Arrays.asList("Иван Иванов", "Светлана Петрова", "Кристина Белова",
+                        "Анна Мусина", "Анна Крутова", "Иван Юрин", "Петр Лыков", "Павел Чернов", "Петр Чернышов",
+                        "Мария Федорова", "Марина Светлова", "Мария Савина", "Мария Рыкова", "Марина Лугова",
+                        "Анна Владимирова", "Иван Мечников", "Петр Петин", "Иван Ежов"));
+
+    Map<String, Integer> words = new HashMap<>();
+
 }
